8814a5ff14048202255123db66b84eea396b8a4b
Eduardo-aSantos <eduardo.isdeveloper@gmail.com>
2025-05-15 18:47:24 -0300

    Update contact-form.tsx
    Correção paliativa do envio do formulario

--- src/components/sections/contact-form.tsx
+++ src/components/sections/contact-form.tsx
@@ -94,8 +94,8 @@ export const ContactForm = () => {
       // Criando o objeto de dados para enviar o email no formato exato que a API SMTP2GO espera
       const emailData = {
         api_key: "api-B2D23EE73D9343BB89154580ECDDBD2E",
-        to: [`Global Vortex - Website <${import.meta.env.VITE_EMAIL_CONTACT}>`],
-        sender: `Global Vortex - Website <${import.meta.env.VITE_EMAIL_CONTACT}>`,
+        to: [`Global Vortex - Website <contact@globalvortexs.com>`],
+        sender: `Global Vortex - Website <contact@globalvortexs.com>`,
         template_id: "7468142",
         template_data: {
           fullName: formData.name,
